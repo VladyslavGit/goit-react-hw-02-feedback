9da73c4e693e1bb2d4b00ff63ced022db19307e6
Vladyslav <vladyslav.work@gmail.com>
2020-03-31 14:04:41 +0200

    Updates

---
+++ precache-manifest.b781af67b91ae8fa3ef9a15057d5757e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f7b8c69efbe86aa2de393b737d029c",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0a71e76819b33e2bd2a5",
+    "url": "/goit-react-hw-02-feedback/static/css/main.da274b2a.chunk.css"
+  },
+  {
+    "revision": "290a5d0fd13ced7b39c9",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4dbb0468.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4dbb0468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a71e76819b33e2bd2a5",
+    "url": "/goit-react-hw-02-feedback/static/js/main.ede801ae.chunk.js"
+  },
+  {
+    "revision": "7e705c9aa8b1c3132313",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.04217bb5.js"
+  }
+]);
